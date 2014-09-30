a92e0083ad8f2b8dea43e33a690f434cedddb65f
Paul Rondeau <parondea@uwaterloo.ca>
2014-09-30 11:35:32 +0200

    almost done assignment 1 part 1, fixing error with knap_10

--- 3B/alg/assignment1/assignment1.py
+++ 3B/alg/assignment1/assignment1.py
@@ -8,34 +8,44 @@ def binaryBruteForce(n, M, W, C):
 	# finite set C = {c1, c2, … ,cn } (costs of items)
 	count = 0
 	maxCost = 0
-	print "n=", 2**n
-	print 'M=', M
-	print 'W=',W
-	print 'C=', C
 	while count < 2**n:
 		binCount = [int(l) for l in bin(count)[2:]]
+		while len(binCount) < n:
+			binCount.insert(0, 0)
+		# print "count = ", count, "bincount = ", binCount
 		curWeight = 0
 		curCost = 0
 		for index, val in enumerate(binCount):
 			if val == 1:
 				curWeight += W[index]
 				curCost += C[index]
+				# print "w, c", curWeight, curCost
 		if curCost > maxCost and curWeight <= M:
 			maxCost = curCost
 			X = binCount
-			while len(X) < n:
-				X.append(0)
+			# print "maxCost", maxCost
 		count += 1
 
-	print 'maxCost=', maxCost
-	print 'X=', X
-	return
+	# print 'maxCost=', maxCost
+	# print 'X=', X
+	myAns.append(''.join(map(str, X)))
+	if len(myAns) == len(ans):
+		# print "myAns=", myAns
+		# print "ans=", ans
+		for index, element in enumerate(ans):
+			if ans[index] != myAns[index]:
+				print "index", index, "does not work"
+				print myAns[index]
+		if myAns == ans:
+			print "it's fuckin fine"
 
-# binaryBruteForce(5,10,[1,10,3,4,5],[5,4,3,2,1])
-for line in open('sol/knap_4.sol.dat', 'r')
+ans = []
+myAns = []
+for line in open('sol/knap_10.sol.dat', 'r'):
+	lineArr = line.rstrip().split(' ')
+	ans.append(''.join(lineArr[3:len(lineArr)]))
 
-
-for line in open('inst/knap_4.inst.dat', 'r'):
+for line in open('inst/knap_10.inst.dat', 'r'):
 	lineArr = line.rstrip().split(' ')
 	n = int(lineArr[1])
 	M = int(lineArr[2])
@@ -43,44 +53,8 @@ for line in open('inst/knap_4.inst.dat', 'r'):
 	C = []
 	i = 0
 	while i < n:
-		W.append(int(lineArr[i+3]))
-		C.append(int(lineArr[n+i+3]))
+		W.append(int(lineArr[2*i+3]))
+		C.append(int(lineArr[2*i+3+1]))
 		i += 1
-	print n,M,W,C
 
 	binaryBruteForce(n, M, W, C)
-
-
-
-
-# M = 0
-# totalWeight = 0
-# totalCost = 0
-# finalX = []
-# maxCost = 0
-
-
-# def brute(n, M, W, C):
-# 	X = [0] * n
-# 	recursiveCheck(n, W, C, X)
-# 	print finalX
-# 	return True
-
-# def recursiveCheck(n, W, C, X):
-# 	print X
-# 	for index, item in enumerate(W):
-# 		if X[index] == 0:
-
-# 			print index
-
-# 			if totalWeight + W[index] < M:
-# 				totalWeight += W[index]
-# 				totalCost += C[index]
-# 				X[index] = 1
-# 			recursiveCheck(n, W, C, X)
-# 		elif index == n - 1:
-# 			if totalCost > maxCost:
-# 				maxCost = totalCost
-# 				finalX = X
-# 			X = [0] * n
-# 			break
