0b7445dbcac7b155cfc7a64cd0164f948d5957bf
Paul Rondeau <parondea@uwaterloo.ca>
2014-10-20 16:04:16 +0200

    started assignment2 then realized it is not due for 2 more weeks

---
+++ 3B/alg/assignment2/bandb.py
@@ -0,0 +1,19 @@
+def bAndB(n, M, W, C):
+	print "t"
+
+
+# for line in open('inst/knap_4.inst.dat', 'r'):
+# 	lineArr = line.rstrip().split(' ')
+# 	n = int(lineArr[1])
+# 	M = int(lineArr[2])
+# 	W = []
+# 	C = []
+# 	i = 0
+# 	while i < n:
+# 		W.append(int(lineArr[2*i+3]))
+# 		C.append(int(lineArr[2*i+3+1]))
+# 		i += 1
+
+# 	bAndB(n, M, W, C)
+
+bAndB(4, 100, [18, 42, 88, 3], [114, 136, 192, 223])
