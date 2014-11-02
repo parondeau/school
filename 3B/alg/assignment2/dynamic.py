95612e1c1ac66e13b66e56ffc8536c467424ed00
Paul Rondeau <paulrondeau07@gmail.com>
2014-11-02 20:42:16 +0100

    maybe working dynamic

--- 3B/alg/assignment2/dynamic.py
+++ 3B/alg/assignment2/dynamic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-def bAndB(M, W, C):
+def dynamic(M, W, C):
 
 	# M - knapsack size
 	# W - weight of items undecided
@@ -10,28 +10,28 @@ def bAndB(M, W, C):
 	global totalW
 	global totalC
 	global bestC
+	# print M
 	if len(W) > 0:
-		# pruning time
-		# cost function
-		if totalC + sum(C) > bestC:
-			# item in bag
-			curItemW = W.pop(0)
-			curItemC = C.pop(0)
-			if totalW < M:
-				totalW = totalW + curItemW
-				totalC = totalC + curItemC
+		# item in bag
+		curItemW = W.pop(0)
+		curItemC = C.pop(0)
+		M = M - curItemW
+		totalC = totalC + curItemC
+		dynamic(M, W, C)
+		# item not in bag
 
-				bAndB(M, W, C)
-				# item not in bag
-				totalW = totalW - curItemW
-				totalC = totalC - curItemC
-
-			bAndB(M, W, C)
-			W.insert(0, curItemW)
-			C.insert(0, curItemC)
+		M = M + curItemW
+		totalC = totalC - curItemC
+		dynamic(M, W, C)
+		W.insert(0, curItemW)
+		C.insert(0, curItemC)
 	else:
-		if bestC < totalC and totalW <= M:
+		# print "totalC =", totalC
+		# print "M =", M
+		if M >= 0 and bestC < totalC:
 			bestC = totalC
+
+
 
 totalW = 0
 totalC = 0
@@ -49,6 +49,9 @@ for line in open('inst/knap_4.inst.dat', 'r'):
 		C.append(int(lineArr[2*i+3+1]))
 		i += 1
 
+# M = 100
+# W = [18, 42, 88, 3]
+# C = [114, 136, 192, 223]
 	bestC = 0
-	bAndB(M, W, C)
+	dynamic(M, W, C)
 	print bestC
