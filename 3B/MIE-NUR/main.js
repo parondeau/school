5b73074b667e8b49002d496a9b927fda7baf4f23
Paul Rondeau <paulrondeau07@gmail.com>
2014-11-25 21:57:16 +0100

    finished ui project

--- 3B/MIE-NUR/main.js
+++ 3B/MIE-NUR/main.js
@@ -1,66 +1,58 @@
 $(document).ready(function() {
-	$('#gradientTop').css('height', ($('header').height()));
-	$('main').css('margin-top', ($('header').height()));
-
-	// javascript
-	var dataJS = [
-		{
-			value: 90,
-			color: "#b72200"
-		}, 
-		{
-			value: 10,
-			color: "#eee"
-		}
-	]
-	// python
-	var dataPY = [
-		{
-			value: 75,
-			color: "#b72200"
-		}, 
-		{
-			value: 25,
-			color: "#eee"
-		}
-	]
-	// HTML/CSS
-	var dataHTML = [
-		{
-			value: 95,
-			color: "#b72200"
-		}, 
-		{
-			value: 5,
-			color: "#eee"
-		}
-	]
-	// c
-	var dataC = [
-		{
-			value: 40,
-			color: "#b72200"
-		}, 
-		{
-			value: 60,
-			color: "#eee"
-		}
-	]
-	var options = {
-		percentageInnerCutout: 90,
-	    animation: false,
-	    responsive: true
-	}
-	var ctx = $("#skillChartJS").get(0).getContext("2d");
-	var myDoughnutChart = new Chart(ctx).Doughnut(dataJS, options);
-
-	var ctx = $("#skillChartPY").get(0).getContext("2d");
-	var myDoughnutChart = new Chart(ctx).Doughnut(dataPY, options);
-
-	var ctx = $("#skillChartHTML").get(0).getContext("2d");
-	var myDoughnutChart = new Chart(ctx).Doughnut(dataHTML, options);
-
-	var ctx = $("#skillChartC").get(0).getContext("2d");
-	var myDoughnutChart = new Chart(ctx).Doughnut(dataC, options);
-
+	// setupMaps();
+	setupClicks();
 });
+
+// function setupMaps() {
+// 	var useragent = navigator.userAgent;
+// 	var mapdiv = document.getElementById("map-canvas");
+
+// 	if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
+// 		mapdiv.style.width = '100%';
+// 		mapdiv.style.height = '100%';
+// 	} else {
+// 		mapdiv.style.width = '600px';
+// 		mapdiv.style.height = '800px';
+// 	}
+// 	var mapOptions = {
+// 		center: { lat: 50.0833, lng: 14.4167},
+// 		zoom: 15
+// 	};
+// 	var map = new google.maps.Map(mapdiv, mapOptions);
+// }
+function setupClicks() {
+	$('#searchTab').on('click touchstart', function(){
+		$('#tabBar *').removeClass('show');
+		$('#searchTab *').addClass('show');
+		$('#content > *').hide();
+		$('#mainSearch').show();
+	});
+	$('#historyTab').on('click touchstart', function(){
+		$('#tabBar *').removeClass('show');
+		$('#historyTab *').addClass('show');
+		$('#content > *').hide();
+		$('#mainHistory').show();
+	});
+	$('#favTab').on('click touchstart', function(){
+		$('#tabBar *').removeClass('show');
+		$('#favTab *').addClass('show');
+		$('#content > *').hide();
+		$('#mainFav').show();
+	});
+	$('#listTab').on('click touchstart', function(){
+		$('#tabBar *').removeClass('show');
+		$('#listTab *').addClass('show');
+		$('#content > *').hide();
+		$('#mainList').show();
+	});
+	$('#mainSearch li').on('click', function(){
+		$('#mainList').hide();
+		$('#booking').show();
+		$('.backButton').show();
+	});
+	$('.backButton').on('click', function(){
+		$('#booking').hide();
+		$('#mainList').show();
+		$('.backButton').hide();
+	});
+}
